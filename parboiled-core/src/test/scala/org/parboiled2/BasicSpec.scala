b2c6732a5b923c44aa6a464c1a485a0b73a42a84
Jeffrey Shaw <shawjef3@gmail.com>
2015-09-19 22:21:41 -0400

    convert BasicSpec

--- parboiled-core/src/test/scala/org/parboiled2/BasicSpec.scala
+++ parboiled-core/src/test/scala/org/parboiled2/BasicSpec.scala
@@ -16,6 +16,7 @@
 
 package org.parboiled2
 
+import scodec.bits.ByteVector
 import shapeless.test.illTyped
 import org.specs2.specification.Scope
 
@@ -23,184 +24,184 @@ class BasicSpec extends TestParserSpec {
 
   "The Parser should correctly recognize/reject input for" >> {
 
-    "simple char literals" in new TestParser0 {
-      val targetRule = rule { 'x' }
-      "x" must beMatched
-      "y" must beMismatched
-      "" must beMismatched
+    "simple Byte literals" in new TestParser0 {
+      val targetRule = rule { 1.toByte }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
-    "a simple char `val`" in new TestParser0 {
-      val c = 'x'
+    "a simple Byte `val`" in new TestParser0 {
+      val c = 1.toByte
       val targetRule = rule { c }
-      "x" must beMatched
-      "y" must beMismatched
-      "" must beMismatched
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
-    "a simple char `def`" in new TestParser0 {
-      def c = 'x'
+    "a simple Byte `def`" in new TestParser0 {
+      def c: Byte = 1
       val targetRule = rule { c }
-      "x" must beMatched
-      "y" must beMismatched
-      "" must beMismatched
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
-    "simple string literals" in new TestParser0 {
-      val targetRule = rule { "ab" ~ EOI }
-      "" must beMismatched
-      "a" must beMismatched
-      "ab" must beMatched
-      "abc" must beMismatched
+    "simple boxed Byte literals" in new TestParser0 {
+      val targetRule = rule { java.lang.Byte.valueOf(1.toByte) }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
-    "a simple string `val`" in new TestParser0 {
-      val s = "ab"
-      val targetRule = rule { s ~ EOI }
-      "" must beMismatched
-      "a" must beMismatched
-      "ab" must beMatched
-      "abc" must beMismatched
+    "a simple boxed Byte `val`" in new TestParser0 {
+      val c = java.lang.Byte.valueOf(1.toByte)
+      val targetRule = rule { c }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
-    "a simple string `def`" in new TestParser0 {
-      def s = "ab"
-      val targetRule = rule { s ~ EOI }
-      "" must beMismatched
-      "a" must beMismatched
-      "ab" must beMatched
-      "abc" must beMismatched
+    "a simple boxed Byte `def`" in new TestParser0 {
+      def c = java.lang.Byte.valueOf(1.toByte)
+      val targetRule = rule { c }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
+    }
+
+    "simple Int literals" in new TestParser0 {
+      val targetRule = rule { 1 }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
+    }
+
+    "a simple Int `val`" in new TestParser0 {
+      val c = 1
+      val targetRule = rule { c }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
+    }
+
+    "a simple Int `def`" in new TestParser0 {
+      def c = 1
+      val targetRule = rule { c }
+      ByteVector(1) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
     "a CharPredicate" in new TestParser0 {
-      val targetRule = rule { CharPredicate.Digit }
-      "0" must beMatched
-      "8" must beMatched
-      "x" must beMismatched
-      "" must beMismatched
+      val targetRule = rule { BytePredicate.Digit }
+      ByteVector('0'.toByte) must beMatched
+      ByteVector('8'.toByte) must beMatched
+      ByteVector(0) must beMismatched
+      ByteVector.empty must beMismatched
     }
 
     "anyOf" in new TestParser0 {
-      val targetRule = rule { anyOf("abc") ~ EOI }
-      "" must beMismatched
-      "a" must beMatched
-      "b" must beMatched
-      "c" must beMatched
-      "d" must beMismatched
-      "ab" must beMismatched
+      val targetRule = rule { anyOf(0, 1, 2) ~ EOI }
+      ByteVector.empty must beMismatched
+      ByteVector(0) must beMatched
+      ByteVector(1) must beMatched
+      ByteVector(2) must beMatched
+      ByteVector(3) must beMismatched
+      ByteVector(0, 1) must beMismatched
     }
 
     "noneOf" in new TestParser0 {
-      val targetRule = rule { noneOf("abc") ~ EOI }
-      "" must beMismatched
-      "a" must beMismatched
-      "b" must beMismatched
-      "c" must beMismatched
-      "d" must beMatched
-      "ab" must beMismatched
-    }
-
-    "ignoreCase(char)" in new TestParser0 {
-      val targetRule = rule { ignoreCase('x') ~ EOI }
-      "" must beMismatched
-      "x" must beMatched
-      "X" must beMatched
-      "y" must beMismatched
-    }
-
-    "ignoreCase(string)" in new TestParser0 {
-      val targetRule = rule { ignoreCase("ab") ~ EOI }
-      "" must beMismatched
-      "a" must beMismatched
-      "ab" must beMatched
-      "Ab" must beMatched
-      "aB" must beMatched
-      "abc" must beMismatched
+      val targetRule = rule { noneOf(0, 1, 2) ~ EOI }
+      ByteVector.empty must beMismatched
+      ByteVector(0) must beMismatched
+      ByteVector(1) must beMismatched
+      ByteVector(2) must beMismatched
+      ByteVector(3) must beMatched
+      ByteVector(0, 1) must beMismatched
     }
 
     "ANY" in new TestParser0 {
       val targetRule = rule { ANY }
-      "a" must beMatched
-      "Ж" must beMatched
-      "" must beMismatched
+      ByteVector(0) must beMatched
+      ByteVector(Byte.MinValue) must beMatched
+      ByteVector.empty must beMismatched
     }
 
     "EOI" in new TestParser0 {
       val targetRule = rule { EOI }
-      "" must beMatched
-      "x" must beMismatched
+      ByteVector.empty must beMatched
+      ByteVector(0) must beMismatched
     }
 
-    "character ranges" in new TestParser0 {
-      val targetRule = rule { ("1" - "5") ~ EOI }
-      "1" must beMatched
-      "3" must beMatched
-      "5" must beMatched
-      "" must beMismatched
-      "0" must beMismatched
-      "a" must beMismatched
-      "8" must beMismatched
+    "byte ranges" in new TestParser0 {
+      val targetRule = rule { (1 - 5) ~ EOI }
+      ByteVector(1) must beMatched
+      ByteVector(3) must beMatched
+      ByteVector(5) must beMatched
+      ByteVector.empty must beMismatched
+      ByteVector(0) must beMismatched
+      ByteVector(-1) must beMismatched
+      ByteVector(6) must beMismatched
     }
 
     "MATCH" in new TestParser0 {
       val targetRule = rule { MATCH ~ EOI }
-      "" must beMatched
-      "x" must beMismatched
+      ByteVector.empty must beMatched
+      ByteVector(0) must beMismatched
     }
 
     "rule calls to `val`s" in new TestParser0 {
-      val targetRule = rule { (val0 | val1(1) | val2(2, '2') | val3(3, "s", 'x')) ~ EOI }
-      val val0 = rule { '0' }
-      val val1 = rule[Int]() { _ times '1' }
-      val val2 = rule[Int, Char]() { _ times _ }
-      val val3 = rule[Int, String, Char]() { (i, s, c) ⇒ i times (s + c) }
-      "0" must beMatched
-      "1" must beMatched
-      "22" must beMatched
-      "sxsxsx" must beMatched
+      val targetRule = rule { (val0 | val1(1) | val2(2, 2) | val3(3, ByteVector(3), 4)) ~ EOI }
+      val val0 = rule { 0 }
+      val val1 = rule[Int]() { _ times 1 }
+      val val2 = rule[Int, Byte]() { _ times _ }
+      val val3 = rule[Int, ByteVector, Byte]() { (i, s, b) ⇒ i times (s :+ b) }
+      ByteVector(0) must beMatched
+      ByteVector(1) must beMatched
+      ByteVector(2, 2) must beMatched
+      ByteVector(3, 4, 3, 4, 3, 4) must beMatched
     }
 
     "rule calls to `def`s with inner parameters" in new TestParser0 {
-      val targetRule = rule { (def0 | def1(1) | def2(2, '2') | def3(3, "s", 'x')) ~ EOI }
-      def def0 = rule { '0' }
-      def def1 = rule[Int]() { _ times '1' }
-      def def2 = rule[Int, Char]() { _ times _ }
-      def def3 = rule[Int, String, Char]() { (i, s, c) ⇒ i times (s + c) }
-      "0" must beMatched
-      "1" must beMatched
-      "22" must beMatched
-      "sxsxsx" must beMatched
+      val targetRule = rule { (def0 | def1(1) | def2(2, 2) | def3(3, ByteVector(3), 4)) ~ EOI }
+      def def0 = rule { 0 }
+      def def1 = rule[Int]() { _ times 1 }
+      def def2 = rule[Int, Byte]() { _ times _ }
+      def def3 = rule[Int, ByteVector, Byte]() { (i, s, b) ⇒ i times (s :+ b) }
+      ByteVector(0) must beMatched
+      ByteVector(1) must beMatched
+      ByteVector(2, 2) must beMatched
+      ByteVector(3, 4, 3, 4, 3, 4) must beMatched
     }
 
     "rule calls to `def`s with outer parameters" in new TestParser0 {
-      val targetRule = rule { (def0 | def1(1) | def2(2, '2') | def3(3, "s", 'x')) ~ EOI }
+      val targetRule = rule { (def0 | def1(1) | def2(2, 2) | def3(3, ByteVector(3), 4)) ~ EOI }
       def def0 = rule { '0' }
-      def def1(i: Int) = rule { i times '1' }
-      def def2(i: Int, c: Char) = rule { i times c }
-      def def3(i: Int, s: String, c: Char) = rule { i times (s + c) }
-      "0" must beMatched
-      "1" must beMatched
-      "22" must beMatched
-      "sxsxsx" must beMatched
+      def def1(i: Int) = rule { i times 1 }
+      def def2(i: Int, c: Byte) = rule { i times c }
+      def def3(i: Int, s: ByteVector, c: Byte) = rule { i times (s :+ c) }
+      ByteVector(0) must beMatched
+      ByteVector(1) must beMatched
+      ByteVector(2, 2) must beMatched
+      ByteVector(3, 4, 3, 4, 3, 4) must beMatched
     }
 
-    "Map[String, T]" in new TestParser1[Int] {
-      val colors = Map("red" -> 1, "green" -> 2, "blue" -> 3)
+    "Map[ByteVector, T]" in new TestParser1[Int] {
+      val colors = Map(ByteVector(0) -> 1, ByteVector(-1, 3, 7) -> 2, ByteVector.empty -> 3)
       val targetRule = rule { colors ~ EOI }
-      "red" must beMatchedWith(1)
-      "green" must beMatchedWith(2)
-      "blue" must beMatchedWith(3)
-      "black" must beMismatched
+      ByteVector(0) must beMatchedWith(1)
+      ByteVector(-1, 3, 7) must beMatchedWith(2)
+      ByteVector.empty must beMatchedWith(3)
+      ByteVector(-1) must beMismatched
     }
   }
 
   "The Parser" should {
-    "disallow compilation of an illegal character range" in new SimpleParser with Scope {
-      illTyped("""rule { "00" - "5" }""", "lower bound must be a single char string")
-      illTyped("""rule { "0" - "55" }""", "upper bound must be a single char string")
-      illTyped("""rule { "" - "5" }""", "lower bound must be a single char string")
-      illTyped("""rule { "0" - "" }""", "upper bound must be a single char string")
-      illTyped("""rule { "5" - "1" }""", "lower bound must not be > upper bound")
+    "disallow compilation of an illegal byte range" in new SimpleParser with Scope {
+      illTyped(s"""rule { ${Byte.MinValue.toInt - 1} - 0""", "lower bound must be a byte")
+      illTyped(s"""rule { 0 - ${Byte.MaxValue.toInt + 1}""", "upper bound must be a byte")
+      illTyped("""rule { 5 - 1 }""", "lower bound must not be > upper bound")
       success
     }
   }
